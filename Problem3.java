edee4de46d89a3104ad3f2b4d65454ead4c404ed
Syed Shoaib Sahah <shoaibshah2400@gmail.com>
2025-02-07 19:06:15 -0500

    done with twopointers-2

---
+++ Problem3.java
@@ -0,0 +1,20 @@
+// time complexity o(m*n)
+// space complexity o(1)
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int i = 0; int j = n-1;
+        while(i < m && j >=0){
+            if(matrix[i][j] == target) return true;
+            if(matrix[i][j] < target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return false;
+    }
+}
